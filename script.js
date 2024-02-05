5fbf923fd077c29a80fa77fa0fd382026837dd56
Walison <walison.cardoso@proton.me>
2024-02-05 20:39:11 -0300

    Criptografia Completa

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ function codificarTexto() {
     let plainText = document.getElementById('texto-entrada').value;
     let plainTextArray = Array.from(plainText);
     let plainTextFinal = '';
- 
+
     for (let i = 0; i < plainTextArray.length; i++) {
         if (plainTextArray[i] == 'a') {
-            // plainTextFinal.concat(plainTextArray[i]);
-            console.log('tem letra a')
-        }     
+            plainTextFinal = plainTextFinal.concat('ai');
+            } else if (plainTextArray[i] == 'e') {
+            plainTextFinal = plainTextFinal.concat('enter');
+            } else if (plainTextArray[i] == 'i') {
+                plainTextFinal = plainTextFinal.concat('imes');
+            } else if (plainTextArray[i] == 'o') {
+                plainTextFinal = plainTextFinal.concat('ober');
+            } else if (plainTextArray[i] == 'u') {
+                plainTextFinal = plainTextFinal.concat('ufat');
+            } else {
+                plainTextFinal = plainTextFinal.concat(plainTextArray[i]);
+            }     
     }
-
+    console.log(plainTextFinal)
 }
 
 function decodificarTexto() {
@@ -21,4 +30,5 @@ function decodificarTexto() {
     for (let j = 0; j < cipherTextArray.length; j++) {
         console.log(j)
     }
+    console.log(plainTextFinal)
 }
