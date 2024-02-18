cf6ae13366751e17d1da6f016b29957277da292e
Walison <walison.cardoso@proton.me>
2024-02-18 14:49:15 -0300

    Botao Copiar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let plainTextFinal = '';
+let cipherTextFinal = '';
+
 function hideOutputText() {
     document.getElementById("botao-output").style.display = "";
     document.getElementById("textoResultado").removeAttribute("hidden");
@@ -16,51 +19,55 @@ function botaoLimparTexto() {
     document.getElementById("titulo").style.display = "";
     document.getElementById("paragrafo").style.display = "";
     document.getElementById("textoResultado").hidden = true;
+    document.getElementById("textoResultado").innerHTML = "";
     document.getElementById("botao-output").style.display = "none";
     document.getElementById("texto-entrada").value = "";
 }
 
 function botaoCopiarTexto() {
-    console.log(plainTextFinal)
-
-    
+    if (plainTextFinal == "") {
+        navigator.clipboard.writeText(cipherTextFinal); 
+    } else {
+        navigator.clipboard.writeText(plainTextFinal); 
+    }
     alert("Texto copiado para a área de transferência!");
 }
 
 function codificarTexto() { // Funcao codificar - Analisa letra por letra e troca as vogais pelo codigo correspondente
     let plainText = document.getElementById('texto-entrada').value;
     let plainTextArray = Array.from(plainText);
-    let plainTextFinal = '';
     let textoEntrada = document.getElementById("texto-entrada").value
+    plainTextFinal = "";
+    cipherTextFinal = "";
 
     if (textoEntrada == "") {
         alert(" Digite o texto para ser criptografado ou descriptografado!")
     } else {
-                for (let i = 0; i < plainTextArray.length; i++) {
-                    if (plainTextArray[i] == 'a') {
-                    plainTextFinal = plainTextFinal.concat('ai');
-                    } else if (plainTextArray[i] == 'e') {
-                    plainTextFinal = plainTextFinal.concat('enter');
-                    } else if (plainTextArray[i] == 'i') {
-                        plainTextFinal = plainTextFinal.concat('imes');
-                    } else if (plainTextArray[i] == 'o') {
-                        plainTextFinal = plainTextFinal.concat('ober');
-                    } else if (plainTextArray[i] == 'u') {
-                        plainTextFinal = plainTextFinal.concat('ufat');
-                    } else {
-                        plainTextFinal = plainTextFinal.concat(plainTextArray[i]);
-                    }           
-                }
-                hideOutputText();
-                exibeMensagem(plainTextFinal);
-                return plainTextFinal;
+        for (let i = 0; i < plainTextArray.length; i++) {
+            if (plainTextArray[i] == 'a') {
+                plainTextFinal = plainTextFinal.concat('ai');
+                } else if (plainTextArray[i] == 'e') {
+                plainTextFinal = plainTextFinal.concat('enter');
+                } else if (plainTextArray[i] == 'i') {
+                    plainTextFinal = plainTextFinal.concat('imes');
+                } else if (plainTextArray[i] == 'o') {
+                    plainTextFinal = plainTextFinal.concat('ober');
+                } else if (plainTextArray[i] == 'u') {
+                    plainTextFinal = plainTextFinal.concat('ufat');
+                } else {
+                    plainTextFinal = plainTextFinal.concat(plainTextArray[i]);
+                }          
             }
+            hideOutputText();            
+            exibeMensagem(plainTextFinal);
+        }
 }
 
 function decodificarTexto() { // Funcao decodificar - Usa o metodo replace() para trocar a parte codificada do texto para a vogal correspondente
     let cipherText = document.getElementById('texto-entrada').value;
-    let cipherTextFinal = '';
     let textoEntrada = document.getElementById("texto-entrada").value
+    cipherTextFinal = "";
+    plainTextFinal = "";
 
     if (textoEntrada == "") {
         alert(" Digite o texto para ser criptografado ou descriptografado!")
